a3e879a6ccebcc6d6e5541fbd4b48c43848babd4
Quintensevinga <quintensevinga@outlook.com>
2023-09-29 14:59:52 +0200

    set-up integration test

---
+++ test/integration/controller.test.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import router from '../../server/router';
+import supertest from 'supertest';
+import Dish from '../../server/models/Dish'
+
+import mongoose from 'mongoose';
+const databaseName = 'test';
+
+describe('integration tests', () => {
+
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+  const request = supertest(app);
+
+  beforeAll(async () => {
+    const url = `mongodb://localhost:27017/${databaseName}`;
+    await mongoose.connect(url);
+  })
+
+  afterEach(async () => {
+    await Dish.deleteMany();
+  })
+})
