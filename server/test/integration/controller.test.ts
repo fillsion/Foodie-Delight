e1c7d63beadaeb85296e1edd4e766e5185de0175
Quintensevinga <quintensevinga@outlook.com>
2023-09-30 15:51:11 +0200

    integration test; get liked dishes

--- server/test/integration/controller.test.ts
+++ server/test/integration/controller.test.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import router from '../../router';
 import supertest from 'supertest';
 import Dish from '../../models/Dish';
@@ -15,12 +15,17 @@ describe('integration tests', () => {
 
   beforeAll(async () => {
     const url = `mongodb://localhost:27017/${databaseName}`;
-    await mongoose.connect(url)    
+    await mongoose.connect(url) 
+      .then(() => {
+        console.log('connected to MongoDB')
+      })
+      .catch((error) => {
+        console.error('Failed to connect to MongoDB:', error);
+      });
   })
 
   afterEach(async () => {
     await Dish.deleteMany();
-    mongoose.disconnect();
   })
 
   it('it should save a liked dish to the database', async () => {
@@ -41,4 +46,31 @@ describe('integration tests', () => {
     const savedDish = await Dish.findOne({ title: 'Test Dish2' });
     expect(savedDish).toBeTruthy();
   })
+
+  it('it should get all liked dishes', async () => {
+    const likedDishes = [
+      {
+        title: 'Test Dish2',
+        image: 'test-image-url2',
+        summary: 'Test summary2',
+        instructions: 'Test instructions2',
+        liked: true 
+      },
+      {
+        title: 'Test Dish3',
+        image: 'test-image-url3',
+        summary: 'Test summary3',
+        instructions: 'Test instructions3',
+        liked: true
+      },
+    ];
+
+    await Dish.insertMany(likedDishes);
+
+    const response = await request.get('/likedDishes').expect(200);
+    
+    expect(response.body.length).toBe(likedDishes.length);
+
+  })
 })
+
