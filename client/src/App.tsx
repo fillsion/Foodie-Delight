976588d39194f6878f275fa8bd05924a141d6aec
Camilo <camilomafioly@gmail.com>
2023-10-03 16:22:21 -0500

    feat: toast errors and error handler in apiServices

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import "./App.css";
@@ -10,14 +10,14 @@ import InsForClickedRecipeFromSearch from "./components/InsForClickedRecipeFromS
 import MyFavorites from "./components/MyFavorites";
 
 import { fetchRandomDishes } from "./apiServices/apiServices";
-import { RndDish } from './interfaces/general';
+import { RndDish } from "./interfaces/general";
+import { ErrorContext } from "./context/Error";
 
 function App() {
   const [recipes, setRecipes] = useState<RndDish[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [favorites, setFavorites] = useState<RndDish[]>([]);
-  const [recipesThatAreLiked] = useState<RndDish[]>([]);
 
+  const { showError } = useContext(ErrorContext);
   useEffect(() => {
     async function fetchData() {
       try {
@@ -25,17 +25,12 @@ function App() {
         setRecipes(data);
         setIsLoading(false);
       } catch (err) {
-        console.error(err);
+        showError(err);
         setIsLoading(false);
       }
     }
 
     fetchData();
-  }, []);
-
-  useEffect(() => {
-    const savedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
-    setFavorites(savedFavorites);
   }, []);
 
   return (
@@ -63,9 +58,11 @@ function App() {
           <Route path="/random-dish" element={<RandomDish />} />
           <Route
             path="/my-favorites"
-            element={<MyFavorites
+            element={
+              <MyFavorites
               // recipesThatAreLiked={recipesThatAreLiked}
-              />}
+              />
+            }
           />
           <Route
             path="/ingredient/:ingredient/*"
--- client/src/apiServices/apiServices.ts
+++ client/src/apiServices/apiServices.ts
@@ -1,15 +1,29 @@
 import React from "react";
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Recipe, ProductDetails, RndDish } from "../interfaces/general";
-const apiKey = "e01d44ef8a69456a904614af30d94e79";
 
-export async function fetchRandomDishes(): Promise<RndDish[]> {
-  try {
-    const response = await axios.get<RndDish[]>("http://localhost:4242/random-dishes");
-    return response.data;
-  } catch (err) {
-    console.error(err);
+const apiKey = "337d1853f27a4c559c6e9f124a823ccb";
+
+const handleErrorMessage = (error: any): string => {
+  const axiosError = error as AxiosError;
+  if (axiosError.response) {
+    return axiosError.response.statusText;
   }
+  //TODO verify backend response message
+  return error.message;
+};
+
+async function handleRequest<T>(fetch: Promise<AxiosResponse<T>>): Promise<T> {
+  try {
+    const response = await fetch;
+    return response.data;
+  } catch (err: any) {
+    return Promise.reject(handleErrorMessage(err));
+  }
+}
+
+export function fetchRandomDishes(): Promise<RndDish[]> {
+  return handleRequest(axios.get<RndDish[]>("http://localhost:4242/random-dishes"));
 }
 
 export async function handleLikeClick(
@@ -17,43 +31,32 @@ export async function handleLikeClick(
   setLikedRecipes: React.Dispatch<React.SetStateAction<Record<string, boolean>>>
 ) {
   try {
-    await axios.post(`http://localhost:4242/likedDishes`, recipe);
+    await handleRequest(axios.post("http://localhost:4242/likedDishes", recipe));
     setLikedRecipes((prevLikedRecipes) => ({
       ...prevLikedRecipes,
       [recipe.title]: !prevLikedRecipes[recipe.title],
     }));
   } catch (err) {
-    console.log("Error", err);
+    console.error("Error", err);
   }
 }
 
-export async function fetchRecipesByIngredient(ingredient: string): Promise<Recipe[]> {
-  try {
-    console.log(ingredient);
-    const response = await axios.get<Recipe[]>(
+export function fetchRecipesByIngredient(ingredient: string): Promise<Recipe[]> {
+  return handleRequest(
+    axios.get<Recipe[]>(
       `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredient}`
-    );
-
-    return response.data;
-  } catch (err) {
-    throw err;
-  }
+    )
+  );
 }
 
-export async function fetchRecipeDetails(recipeId: string): Promise<ProductDetails> {
-  try {
-    const apiUrl = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
-    const response = await axios.get<ProductDetails>(apiUrl);
-    return response.data;
-  } catch (err) {
-    throw err;
-  }
+export function fetchRecipeDetails(recipeId: string): Promise<ProductDetails> {
+  return handleRequest(
+    axios.get<ProductDetails>(
+      `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`
+    )
+  );
 }
 
-export async function removeFromFavorites(dishId: string) {
-  try {
-    await axios.delete(`http://localhost:4242/likedDishes/${dishId}`);
-  } catch (err) {
-    throw err;
-  }
+export function removeFromFavorites(dishId: string): Promise<void> {
+  return handleRequest(axios.delete(`http://localhost:4242/likedDishes/${dishId}`));
 }
